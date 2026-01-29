29d36f5e32a0208c235ddb2759be80c60846f515
Arej02 <khadkaarya20@outlook.com>
2026-01-29 13:45:49 +1030

    feat(hidden_layers): increased number of hidden layers

--- src/model.py
+++ src/model.py
@@ -1,13 +1,20 @@
 import torch.nn as nn
 
 class RegressionMLP(nn.Module):
-    def __init__(self, input_dim, hidden_sizes=(256, 128), dropout=0.25):
+    def __init__(self, input_dim, hidden_sizes=(384, 192, 96), dropout=0.15):
         super().__init__()
         layers = []
         prev = input_dim
+        
         for h in hidden_sizes:
-            layers += [nn.Linear(prev, h), nn.ReLU(), nn.Dropout(dropout)]
+            layers.extend([
+                nn.Linear(prev, h),
+                nn.BatchNorm1d(h),          
+                nn.ReLU(),
+                nn.Dropout(dropout),
+            ])
             prev = h
+        
         layers.append(nn.Linear(prev, 5))
         self.net = nn.Sequential(*layers)
 
